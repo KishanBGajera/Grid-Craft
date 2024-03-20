ab3ebac38cd3dc2da96d35e97cbdb9a3a15fb857
KishanBGajera <kishangajera1411@gmail.com>
2024-03-20 19:12:13 +0530

    add undo-redo functionalities

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,9 @@ let currentColorBlock = document.getElementsByClassName("currentColor")[0];
 
 let colorPicker = document.getElementById("colorPicker");
 
+let undoStack = [];
+let redoStack = [];
+
 colorPicker.addEventListener("input", () => {
     setCurrentColor(colorPicker.value);
 })
@@ -12,6 +15,30 @@ function setCurrentColor(color){
     currentColorBlock.style.backgroundColor = currentColor;
 }
 
-function changeColor(cell){
-    cell.style.backgroundColor = currentColor;
+function changeColor(cell, backgroundColor=currentColor){
+    if(!undoStack[undoStack.length-1]){
+        undoStack.push([cell, cell.style.backgroundColor, currentColor]);
+    }
+    else{
+        if(undoStack[undoStack.length-1][0] != cell && undoStack[undoStack.length-1][1] != currentColor){
+            undoStack.push([cell, cell.style.backgroundColor, currentColor]);
+        }
+    }
+    cell.style.backgroundColor = backgroundColor;
+}
+
+function undo(){
+    if(undoStack.length){
+        let [cell, backgroundColor, lastColor] = undoStack.pop();
+        cell.style.backgroundColor = backgroundColor;
+        redoStack.push([cell, backgroundColor, lastColor]);
+    }
+}
+
+function redo(){
+    if(redoStack.length){
+        let [cell, backgroundColor, lastColor] = redoStack.pop();
+        cell.style.backgroundColor = lastColor;
+        undoStack.push([cell, backgroundColor, lastColor]);
+    }
 }
